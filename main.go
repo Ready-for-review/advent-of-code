f77d68e338de7542aeea3bebaf15b3e1800e30cf
Daniel Mies <daniel.mies@codecentric.de>
2020-12-23 20:38:38 +0100

    solution for 2019 day 01

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
 func CalculateFuel(moduleMass int) int {
 	return moduleMass/3 - 2
 }
@@ -9,4 +18,44 @@ func SumOfFuels(moduleMasses []int) (fuel int) {
 		fuel += CalculateFuel(moduleMass)
 	}
 	return
+}
+
+// ImportNumberList opens the given file and parses its contents (comma separated numbers) to an int array
+func ImportNumberList(file string) ([]int, error) {
+	handle, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer handle.Close()
+	return ToNumberList(handle)
+}
+
+// ToNumberList takes a reader and transform the results of it to a []int
+func ToNumberList(handle io.Reader) (listOfNumbers []int, err error) {
+	scanner := bufio.NewScanner(handle)
+	for scanner.Scan() {
+		number, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		listOfNumbers = append(listOfNumbers, number)
+	}
+	return listOfNumbers, nil
+}
+
+func CalculateFuelFromInputFile(input string) (int, error) {
+	moduleMasses, err := ImportNumberList(input)
+	if err != nil {
+		return 0, err
+	}
+
+	return SumOfFuels(moduleMasses), nil
+}
+
+func main() {
+	solution, err := CalculateFuelFromInputFile("./input.txt")
+	if err != nil {
+		log.Fatal("Couldn't parse input file")
+	}
+	fmt.Printf("needed fuel: %d", solution)
 }
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -29,5 +30,23 @@ func Test_SumOfFuels(t *testing.T) {
 	input := []int{12, 14, 1969, 100756}
 	want := 34241
 	got := SumOfFuels(input)
+	assert.Equal(t, want, got)
+}
+
+func Test_ToNumberList(t *testing.T) {
+	t.Run("Test that list of numbers is transformed as expected", func(t *testing.T) {
+		want := []int{1, 2, 3}
+		got, err := ToNumberList(strings.NewReader("1\n2\n3"))
+		assert.Nil(t, err)
+		assert.EqualValues(t, want, got)
+
+	})
+}
+
+func Test_CalculateFuelFromInputFile(t *testing.T) {
+	input := "testinput.txt"
+	want := 34241
+	got, err := CalculateFuelFromInputFile(input)
+	assert.Nil(t, err)
 	assert.Equal(t, want, got)
 }
